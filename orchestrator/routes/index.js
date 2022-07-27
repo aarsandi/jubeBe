405f218c4eff8625998d0970a709399e68e1fb59
aarsandi <aarsandi@gmail.com>
2022-07-27 16:40:22 +0700

    u

--- orchestrator/routes/index.js
+++ orchestrator/routes/index.js
@@ -1,21 +1,40 @@
 const router = require("express").Router()
 const Redis = require("ioredis");
+const axios = require('axios')
 
 const redis = new Redis();
 
-router.get( '/test',  async (req, res, next) => {
-    try {
-        const dataRes = await redis.get('name')
-        
-        res.status(200).json({
-            message: "success",
-            data: dataRes
-        })
-    } catch (err) {
-        res.status(400).json({
-            err : err
-        })
-    }
+router.get( '/test',  (req, res, next) => {
+
+    redis.get('product').then(data => {
+        if(data) {
+            res.status(200).json({
+                message: "success",
+                data: JSON.parse(data)
+            })
+            
+        }else{
+            axios.get('http://141.136.47.149/management/products' , { headers: {"Authorization" : `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiZWFyZXIiLCJzdWIiOjE0MiwiaWF0IjoxNjU4NDEwNDA5LCJleHAiOjE2NTkwMTUyMDl9.xlYBVrLRAf39tx1VrxSP7XVkl-JPhfOp2h15tR9kSdA`} })
+            .then(({ data }) => {
+                if(data) {
+                    redis.set('product', JSON.stringify(data.data))
+                    res.status(200).json({
+                        message: "success",
+                        data: data.data
+                    })
+                }else{
+                    res.status(404).json({
+                        message: "data not found"
+                    })
+                }
+            })
+            .catch((error) => {
+                res.status(404).json({
+                    message: "data not found"
+                })
+            });
+        }
+    })
 })
 
 router.get(
