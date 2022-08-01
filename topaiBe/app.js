27097872b53641cd185df62dcda539d045f197df
aarsandi <aarsandi@gmail.com>
2022-08-01 15:47:55 +0700

    u

--- topaiBe/app.js
+++ topaiBe/app.js
@@ -30,15 +30,6 @@ app.use('/', routes)
 
 app.use(errorHandler)
 
-app.use((req, res) => {
-    req.setTimeout(500000, function(){
-        // call back function is called when request timed out.
-        res.status(500).json({
-            message: `Oops, the url you are looking for does not exist.`
-        })
-    });
-});
-
 // if route doesnt exist
 app.use((req, res, next) => {
     res.status(404).json({
@@ -52,7 +43,7 @@ const serverApp = server.listen(port, () => {
     console.log(`Server Listen at : ${port}`)
 });
 
-// serverApp.timeout = 60000;
+serverApp.timeout = 500000;
 
 // app.listen(port, () => {
 //     console.log(`Server Listen at : ${port}`)
